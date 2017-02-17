2e87238ff8b8694970f9433bada2996300cbb36c
黄青松 <huangqingsong_5678@hotmail.com>
2017-02-17 10:12:58 +0800

    add project

---
+++ hqs-common-qprogress-android/app/src/main/java/com/hqs/hqs_common_qprogress_android/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hqs.hqs_common_qprogress_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
